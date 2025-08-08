0517c58e05c7a276233f5bf857ad8ee58877202c
reza izadi <rezaizadij2000@gmail.com>
2025-08-08 22:31:51 +0200

    change: make them modular to use them later

---
+++ helper.h
@@ -0,0 +1,11 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static int read_line(char *buf, size_t n);
+static int read_int(const char *prompt, int *out);
+static int read_float(const char *prompt, float *out);
+
+#endif
